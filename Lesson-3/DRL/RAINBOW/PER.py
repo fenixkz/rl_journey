07685dad7772586bd176b285e324fdb1dd3c579c
fenixkz <mazhitovayan@gmail.com>
2025-07-29 15:06:20 +0900

    Fix for numpy 2.0

--- Lesson-3/DRL/RAINBOW/PER.py
+++ Lesson-3/DRL/RAINBOW/PER.py
@@ -172,10 +172,10 @@ class ReplayBuffer(object):
         for i in idxes:
             data = self._storage[i]
             obs_t, action, reward, obs_tp1, done, n = data
-            obses_t.append(np.array(obs_t, copy=False))
-            actions.append(np.array(action, copy=False))
+            obses_t.append(np.asarray(obs_t))
+            actions.append(np.asarray(action))
             rewards.append(reward)
-            obses_tp1.append(np.array(obs_tp1, copy=False))
+            obses_tp1.append(np.asarray(obs_tp1))
             dones.append(done)
             ns.append(n)
         return np.array(obses_t), np.array(actions), np.array(rewards), np.array(obses_tp1), np.array(dones), np.array(ns)
