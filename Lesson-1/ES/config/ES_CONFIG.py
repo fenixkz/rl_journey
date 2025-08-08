69258035f2b1f29c2722ebef08b1ec37221d6949
fenixkz <mazhitovayan@gmail.com>
2025-08-08 16:16:09 +0900

    config upd

--- Lesson-1/ES/config/ES_CONFIG.py
+++ Lesson-1/ES/config/ES_CONFIG.py
@@ -1,8 +1,8 @@
 ES_CONFIG = {
     "training_generations": 500,
     "population_size": 100,
-    "noise_std": 0.05,
-    "learning_rate": 0.01,
+    "noise_std": 0.1,
+    "learning_rate": 0.001,
 }
 
 
@@ -10,5 +10,5 @@ ES_ATARI_CONFIG = {
     "training_generations": 5000,
     "population_size": 50,
     "noise_std": 0.02,
-    "learning_rate": 0.01,
+    "learning_rate": 0.001,
 }
