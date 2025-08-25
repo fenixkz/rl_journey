ed14382b19d7dd58f29e82c09e83e9c0a9d0c4ab
fenixkz <mazhitovayan@gmail.com>
2025-08-26 01:56:34 +0900

    config update

--- Lesson-4/SPG/A2C/config/A2C_CONFIG.py
+++ Lesson-4/SPG/A2C/config/A2C_CONFIG.py
@@ -1,14 +1,14 @@
 A2C_CONFIG = {
-    "num_envs": 5,
+    "num_envs": 16,
     "gamma": 0.99,
     "lr": 5e-4,
     "critic_scale": 0.5,
-    "hidden_dim": 64,
+    "hidden_dim": 512,
     "use_normalization": True,
     "use_entropy": True,
     "entropy_coef": 1e-2,
     "max_steps": 1e7,
-    "num_steps": 20,
+    "num_steps": 128,
     "gae_lambda": 0.95,
     "tau": 0.05,  # Set it to 1 to use only one critic
 }
